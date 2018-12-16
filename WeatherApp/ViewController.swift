8bf47ece66c69f24021ed6068839d7ed597307ae
HajimeHayasaka <lunasa-inion-2.scannlain@i.softbank.jp>
2018-12-17 02:28:10 +0900

    - プロジェクト新規作成。

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by 早坂甫 on 2018/12/17.
+//  Copyright © 2018年 早坂甫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
